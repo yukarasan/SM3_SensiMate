a701e79ea8b87de2657375cf65859a9eb25a8aae
Yusuf Kara <yusufkarasan@hotmail.com>
2022-10-20 20:14:09 +0200

    Created packages and a kotlin file "App"

---
+++ app/src/main/java/com/example/sensiMate/ui/App.kt
@@ -0,0 +1,21 @@
+package com.example.sensiMate
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.sensiMate.ui.theme.SensimateTheme
+
+@Composable
+fun App(
+    // modifier: Modifier = Modifier,
+    // appViewModel: AppViewModel = viewModel()
+) {
+    // val appUiState by appViewModel.uiState.collectAsState()
+}
+
+@Preview(showBackground = true)
+@Composable
+fun AppPreview() {
+    SensimateTheme {
+        App()
+    }
+}
---
+++ app/src/main/java/com/example/sensiMate/ui/screens/HomeScreen.kt
@@ -0,0 +1,2 @@
+package com.example.sensiMate.ui.screens
+
