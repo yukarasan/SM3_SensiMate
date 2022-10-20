d6b3ec9ace962189023a50faf44dbe0109f028c5
Yusuf Kara <yusufkarasan@hotmail.com>
2022-10-20 21:00:58 +0200

    Created an event card.

--- app/src/main/java/com/example/sensiMate/ui/home/Event.kt
+++ app/src/main/java/com/example/sensiMate/ui/home/Event.kt
@@ -1,19 +1,34 @@
 package com.example.sensiMate.ui.home
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Card
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 
 @Composable
-fun Event() {
+fun EventCard() {
     Card(modifier = Modifier.padding(8.dp), elevation = 4.dp) {
-        Column() {
-            Text(text = "Test")
-            Text(text = "Test")
+        Column {
+            EventTitle()
+            
+            Row() {
+                Text(text = "Location ")
+                Column() {
+                    Text(text = "2 km ")
+                    Text(text = "The Circular Lab")
+                }
+            }
+            Text(text = "Processbar")
         }
     }
+}
+
+@Composable
+fun EventTitle() {
+    Text(text = "Coca Cola", fontWeight = FontWeight.ExtraBold)
 }
