434716727df696883879aee19b2bc37daf971370
Yusuf Kara <yusufkarasan@hotmail.com>
2022-10-22 13:05:36 +0200

    Did some things

--- app/src/main/java/com/example/sensimate/ui/home/HomeScreen.kt
+++ app/src/main/java/com/example/sensimate/ui/home/HomeScreen.kt
@@ -1,21 +1,18 @@
 package com.example.sensimate.ui.home
 
-import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.Composable
 
 @Composable
 fun HomeScreen() {
-    Column {
-        QuickEntry()
-        EventCard(
-            title = "Coca Cola",
-            distance = "2 km",
-            address = "The Circular Lab"
-        )
-        EventCard(
-            title = "Coca Cola light med ost og kage",
-            distance = "349991 km",
-            address = "The Cicular lab at Roskilde universitet"
-        )
-    }
+    QuickEntry()
+    EventCard(
+        title = "Coca Cola",
+        distance = "2 km",
+        address = "The Circular Lab"
+    )
+    EventCard(
+        title = "Coca Cola light med ost og kage",
+        distance = "349991 km",
+        address = "The Cicular lab at Roskilde universitet"
+    )
 }
