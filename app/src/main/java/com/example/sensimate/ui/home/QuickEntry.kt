e9d7c5fb8f5b36e49c200f7991a8e404befa1248
Yusuf Kara <yusufkarasan@hotmail.com>
2022-10-22 11:26:47 +0200

    Made the text field according to Figma

--- app/src/main/java/com/example/sensimate/ui/home/QuickEntry.kt
+++ app/src/main/java/com/example/sensimate/ui/home/QuickEntry.kt
@@ -1,15 +1,20 @@
 package com.example.sensimate.ui.home
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Card
-import androidx.compose.material.Text
-import androidx.compose.material.TextField
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.sensimate.model.manropeFamily
@@ -58,14 +63,65 @@ private fun EventInputField() {
     Column(
         modifier = Modifier
             .fillMaxWidth()
-            .padding(start = 20.dp, end = 20.dp, bottom = 20.dp, top = 10.dp)
+            .padding(start = 20.dp, end = 20.dp, bottom = 20.dp, top = 5.dp)
+            .height(50.dp)
     ) {
+        // ---------------------------------------------------------------------------
+        //TODO: Needs state hoisting
         var text by remember { mutableStateOf(TextFieldValue("")) }
+        // ---------------------------------------------------------------------------
         TextField(
             value = text,
             onValueChange = { it -> text = it },
-            label = { Text("Event code") },
-            placeholder = { Text( "Enter event code here to open the survey") }
+            label = { Label() },
+            placeholder = { Placeholder() },
+            textStyle = TextStyle(
+                color = Color.White,
+                fontSize = 12.sp,
+                fontFamily = manropeFamily,
+                fontWeight = FontWeight.Bold
+            ),
+            modifier = Modifier
+                .border(
+                    width = 3.dp,
+                    brush = Brush.horizontalGradient(
+                        listOf(Color(74, 75, 90),
+                        Color(74, 75, 90)
+                        )
+                    ),
+                    shape = RoundedCornerShape(35.dp)
+                )
+                .width(400.dp)
+                .background(Color(74, 75, 90), shape = RoundedCornerShape(35.dp)),
+            colors = TextFieldDefaults.textFieldColors(
+                backgroundColor = Color.Transparent,
+                focusedIndicatorColor = Color.Transparent,
+                unfocusedIndicatorColor = Color.Transparent
+            ),
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+            maxLines = 1 //TODO: maxLines not working. Fix this.
         )
     }
+}
+
+@Composable
+private fun Label() {
+    Text(
+        text = "Enter event code",
+        fontFamily = manropeFamily,
+        fontWeight = FontWeight.Bold,
+        fontSize = 12.sp,
+        color = Color.White
+    )
+}
+
+@Composable
+private fun Placeholder() {
+    Text(
+        text = "Enter event code here to open the survey",
+        fontFamily = manropeFamily,
+        fontWeight = FontWeight.Bold,
+        fontSize = 12.sp,
+        color = Color.White
+    )
 }
