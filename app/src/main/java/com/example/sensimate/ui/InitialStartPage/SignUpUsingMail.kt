5fb75affda310e865435724e969449b5a285e2ba
Hussein El-Zein <husseinelzeinprivat@gmail.com>
2022-10-27 14:27:20 +0200

    ChooseSignUpScreen.kt ui done for now

---
+++ app/src/main/java/com/example/sensimate/ui/InitialStartPage/SignUpUsingMail.kt
@@ -0,0 +1,12 @@
+package com.example.sensimate.ui.InitialStartPage
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Preview(showBackground = true)
+@Composable
+fun SignUpUsingMail() {
+
+    InitialStartBackground()
+
+
+}
