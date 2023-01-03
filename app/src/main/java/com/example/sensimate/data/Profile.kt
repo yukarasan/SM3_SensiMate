8cb16cf0c461468975c7049a85ca4e5c87a18dfc
Hussein El-Zein <husseinelzeinprivat@gmail.com>
2023-01-03 17:22:27 +0100

    profile does get retrieved

---
+++ app/src/main/java/com/example/sensimate/data/Profile.kt
@@ -0,0 +1,9 @@
+package com.example.sensimate.data
+
+data class Profile(
+    var dayBorn: String = "",
+    var gender: String = "",
+    var monthBorn: String = "",
+    var postalCode: String = "",
+    var yearBorn: String = ""
+)
