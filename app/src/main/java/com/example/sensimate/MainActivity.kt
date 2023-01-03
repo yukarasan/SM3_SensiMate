7042b839daeaaee9f168cb733f841b296bd36754
anshjyotsingh <anshjyot1504@gmail.com>
2023-01-03 17:24:58 +0100

    System Bars color change

--- app/src/main/java/com/example/sensimate/MainActivity.kt
+++ app/src/main/java/com/example/sensimate/MainActivity.kt
@@ -1,30 +1,61 @@
 package com.example.sensimate
 
 import android.os.Bundle
+
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.core.view.WindowCompat
-import androidx.core.view.WindowInsetsCompat
-import androidx.core.view.WindowInsetsControllerCompat
+import androidx.compose.material.MaterialTheme
+import androidx.compose.ui.graphics.Color
+import android.view.WindowManager
+import androidx.compose.foundation.layout.Column
 
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.toArgb
+import com.example.sensimate.ui.theme.DarkPurple
+import com.example.sensimate.ui.theme.darkbluegrey
 
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        WindowCompat.setDecorFitsSystemWindows(window, false)
+        window.apply {
+            addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+            statusBarColor = DarkPurple.toArgb()
+        }
+
+        // WindowCompat.setDecorFitsSystemWindows(window, false)
         setContent {
-            hideSystemBars()
+            ChangeSystemBarColor()
             App()
         }
     }
 
-    private fun hideSystemBars() {
+    @Composable
+    private fun ChangeSystemBarColor() {
+        MaterialTheme {
+            Surface(color = DarkPurple) {
+                Column {
+                    Text(text = "Sensimate")
+                }
+            }
+        }
+    }
+}
+
+
+
+
+
+/*
         val windowInsetsController = WindowCompat.getInsetsController(window, window.decorView)
         if (windowInsetsController != null) {
             windowInsetsController.systemBarsBehavior =
                 WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
         }
-        windowInsetsController?.hide(WindowInsetsCompat.Type.systemBars())
+        windowInsetsController?.Colorhide(WindowInsetsCompat.Type.systemBars())
     }
-}
+
+ */
