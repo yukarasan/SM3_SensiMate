cb209ce01134c704d5bf249f9d930c20ee96aa54
Hussein El-Zein <husseinelzeinprivat@gmail.com>
2023-01-04 13:31:32 +0100

    now you can change from int to month name

--- app/src/main/java/com/example/sensimate/data/Firebase.kt
+++ app/src/main/java/com/example/sensimate/data/Firebase.kt
@@ -8,12 +8,7 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.google.firebase.firestore.FirebaseFirestoreException
 import android.widget.Toast
-import androidx.compose.foundation.layout.Column
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.runtime.*
-import androidx.compose.ui.graphics.Color
 import com.example.sensimate.data.Database.fetchListOfEvents
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.database.DataSnapshot
@@ -23,10 +18,9 @@ import com.google.firebase.database.ValueEventListener
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.firestore.ktx.toObject
 import com.google.firebase.ktx.Firebase
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.tasks.await
+import java.util.*
 
 @SuppressLint("StaticFieldLeak")
 val db = Firebase.firestore
@@ -153,13 +147,39 @@ object Database {
 
     } //TODO: Hussein
 
+    fun forgotPassword(
+        email: String,
+        context: Context,
+        showLoading: MutableState<Boolean>,
+    ) {
+        showLoading.value = true
+        auth.setLanguageCode("en") // Set to English
+        auth.sendPasswordResetEmail(email).addOnCompleteListener { task ->
+
+            showLoading.value = false
+
+            if (task.isSuccessful) {
+                Toast.makeText(
+                    context, "Successully sent a recovery e-mail",
+                    Toast.LENGTH_LONG
+                ).show()
+            } else {
+                Toast.makeText(
+                    context, "Failed to send e-mail. Is your e-mail correct?",
+                    Toast.LENGTH_LONG
+                ).show()
+            }
+        }
+
+    }//TODO: Hussein
+
     fun deleteProfile() {
 
     } //TODO: Hussein
 
     fun signOut() {
         auth.signOut()
-    } //TODO: Yusuf & Hussein
+    } //TODO: Hussein
 
     fun editUserProfile() {
 
@@ -230,28 +250,28 @@ object Database {
 
     fun createEmployee() {} //TODO: Anshjyot
 
-fun getSurvey() { //TODO: Anshjyot
-    val questions = mutableListOf<Question>()
-    val database = FirebaseDatabase.getInstance()
-    val ref = database.getReference("questions")
-    ref.addListenerForSingleValueEvent(object : ValueEventListener {
-        override fun onDataChange(snapshot: DataSnapshot) {
-            val questionSnapshots = snapshot.children
-            for (questionSnapshot in questionSnapshots) {
-                val question = questionSnapshot.getValue(Question::class.java)
-                if (question != null) {
-                    questions.add(question)
+    fun getSurvey() { //TODO: Anshjyot
+        val questions = mutableListOf<Question>()
+        val database = FirebaseDatabase.getInstance()
+        val ref = database.getReference("questions")
+        ref.addListenerForSingleValueEvent(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                val questionSnapshots = snapshot.children
+                for (questionSnapshot in questionSnapshots) {
+                    val question = questionSnapshot.getValue(Question::class.java)
+                    if (question != null) {
+                        questions.add(question)
+                    }
                 }
             }
-        }
 
-        override fun onCancelled(error: DatabaseError) {
-            Log.d("error", "getListOfQuestions: $error")
-        }
-    })
-}
+            override fun onCancelled(error: DatabaseError) {
+                Log.d("error", "getListOfQuestions: $error")
+            }
+        })
+    }
 
-data class Question(val text: String, val answers: List<Boolean>)
+    data class Question(val text: String, val answers: List<Boolean>)
 
 
     fun answerQuestion() {  //TODO: Anshjyot
@@ -262,6 +282,7 @@ data class Question(val text: String, val answers: List<Boolean>)
                 val answers = snapshot.children.mapNotNull { it.getValue(Answer::class.java) }
                 updateAnswers(answers)
             }
+
             override fun onCancelled(error: DatabaseError) {
                 Log.d("error", "getListOfAnswers: $error")
             }
@@ -271,10 +292,18 @@ data class Question(val text: String, val answers: List<Boolean>)
     fun updateAnswers(answers: List<Answer>) {
 
     }
-    data class Answer(val questionId: String, val answer: List<Boolean>)
 
+    data class Answer(val questionId: String, val answer: List<Boolean>)
 
 
     fun exportToExcel() {} //TODO: LATER
 
+}
+
+object OurCalendar{
+    fun getMonthName(month: Int): String? {
+        val calendar = Calendar.getInstance()
+        calendar.set(Calendar.MONTH, month)
+        return calendar.getDisplayName(Calendar.MONTH, Calendar.LONG, Locale.getDefault())
+    }
 }
