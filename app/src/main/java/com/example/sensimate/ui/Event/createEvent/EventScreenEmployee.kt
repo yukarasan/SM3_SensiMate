b8a32f289b54bd57e25f7311453de28fd0198ea3
Yusuf Kara <yusufkarasan@hotmail.com>
2023-01-04 14:03:56 +0100

    Event Screen Employee can now take the same events as event screen from the database.

--- app/src/main/java/com/example/sensimate/ui/Event/createEvent/EventScreenEmployee.kt
+++ app/src/main/java/com/example/sensimate/ui/Event/createEvent/EventScreenEmployee.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -14,12 +15,14 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import com.example.sensimate.R
+import com.example.sensimate.data.EventDataViewModel
 import com.example.sensimate.ui.navigation.Screen
 
 @Composable
-fun EventScreenEmployee(navController: NavController) {
+fun EventScreenEmployee(navController: NavController, dataViewModel: EventDataViewModel = viewModel()) {
     Column(
         modifier = Modifier.background(
             Brush.verticalGradient(
@@ -31,11 +34,10 @@ fun EventScreenEmployee(navController: NavController) {
         LazyColumn(
             contentPadding = PaddingValues(bottom = 20.dp),
         ) {
-            /*
-            items(data) { item ->   // TODO: Use the commented version in the future:
-                Item(item)
-            }
-             */
+            val state = dataViewModel.state.value
+
+
+
             item {
                 AddEventImage(navController = navController)
                 TextToCreateEvent()
@@ -43,59 +45,27 @@ fun EventScreenEmployee(navController: NavController) {
 
 
             item { QuickEntry() }
-            item {
-                EventCard(
-                    title = "Coca Cola",
-                    timeOfEvent = "2 km",
-                    address = "The Circular Lab",
-                    //progress = 0.5f,
-                    onClick = { navController.navigate(Screen.EditEvent.route) }
-                )
-            }
-            item {
-                EventCard(
-                    title = "Coca Cola light med ost og kage",
-                    timeOfEvent = "349991 km",
-                    address = "The Cicular lab at Roskilde universitet",
-                    //progress = 0.1f,
-                    onClick = { navController.navigate(Screen.EditEvent.route) }
-                )
-            }
-            item {
-                EventCard(
-                    title = "Coca Cola 1",
-                    timeOfEvent = "2 km",
-                    address = "The Circular Lab",
-                    //progress = 1.0f,
-                    onClick = { navController.navigate(Screen.EditEvent.route) }
-                )
-            }
-            item {
-                EventCard(
-                    title = "Coca Cola 2",
-                    timeOfEvent = "2 km",
-                    address = "The Circular Lab",
-                    //progress = 0.0f,
-                    onClick = { navController.navigate(Screen.EditEvent.route) }
-                )
-            }
-            item {
-                EventCard(
-                    title = "Coca Cola 3",
-                    timeOfEvent = "2 km",
-                    address = "The Circular Lab",
-                    //progress = 0.7f,
-                    onClick = { navController.navigate(Screen.EditEvent.route) }
-                )
-            }
-            item {
-                EventCard(
-                    title = "Coca Cola 4",
-                    timeOfEvent = "2 km",
-                    address = "The Circular Lab",
-                    // progress = 0.3f,
-                    onClick = { navController.navigate(Screen.EditEvent.route) }
-                )
+
+
+            state.events?.let {
+                items(it.toList()) { event ->
+                    EventCard(
+                        title = event.title,
+                        timeOfEvent = event.timeOfEvent,
+                        address = event.location,
+                        onClick = {
+                            navController.navigate(
+                                Screen.ExtendedEventScreen.passArguments(
+                                    time = event.timeOfEvent,
+                                    title = event.title,
+                                    description = event.description,
+                                    allergens = event.allergens,
+                                    location = event.location
+                                )
+                            )
+                        }
+                    )
+                }
             }
         }
     }
