c58a09b4289a677e480a6b610264edb40b442575
Hussein El-Zein <husseinelzeinprivat@gmail.com>
2023-01-10 11:13:15 +0100

    now all (my) strings are in strings.xml and no hardcoding whatsoever are found in my files

--- app/src/main/java/com/example/sensimate/data/Firebase.kt
+++ app/src/main/java/com/example/sensimate/data/Firebase.kt
@@ -8,6 +8,7 @@ import android.widget.Toast
 import androidx.compose.runtime.*
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.sensimate.R
 import com.example.sensimate.data.Database.fetchListOfEvents
 import com.example.sensimate.data.questionandsurvey.MyQuestion
 import com.example.sensimate.ui.Event.createEvent.docId
@@ -100,7 +101,6 @@ object Database {
             if (task.isSuccessful) {
                 user.updateEmail(newEmail).addOnCompleteListener { task ->
                     if (task.isSuccessful) {
-                        Log.d(TAG, "Email updated")
                         Toast.makeText(
                             context,
                             "Successfully updated your email",
@@ -200,7 +200,8 @@ object Database {
                     showLoading.value = false
                     successLoggedIn.value = true
                     Toast.makeText(
-                        context, "Account successfully created",
+                        context,
+                        context.resources.getString(R.string.successAcount),
                         Toast.LENGTH_SHORT
                     ).show()
 
@@ -209,7 +210,8 @@ object Database {
                 } else {
                     showLoading.value = false
                     Toast.makeText(
-                        context, "Authentication failed.",
+                        context,
+                        context.resources.getString(R.string.authFailed),
                         Toast.LENGTH_SHORT
                     ).show()
                 }
@@ -273,14 +275,16 @@ object Database {
                     showLoading.value = false
                     successLoggedIn.value = true
                     Toast.makeText(
-                        context, "Account successfully logged in",
+                        context,
+                        context.resources.getString(R.string.successLogged),
                         Toast.LENGTH_SHORT
                     ).show()
 
                 } else {
                     showLoading.value = false
                     Toast.makeText(
-                        context, "Log in failed.",
+                        context,
+                        context.resources.getString(R.string.loginFail),
                         Toast.LENGTH_SHORT
                     ).show()
                 }
@@ -292,12 +296,14 @@ object Database {
         auth.signInAnonymously().addOnCompleteListener { task ->
             if (task.isSuccessful) {
                 Toast.makeText(
-                    context, "Successfully logged in anonymously",
+                    context,
+                    context.resources.getString(R.string.guestSuccessful),
                     Toast.LENGTH_SHORT
                 ).show()
             } else {
                 Toast.makeText(
-                    context, "Log in failed.",
+                    context,
+                    context.resources.getString(R.string.loginFail),
                     Toast.LENGTH_SHORT
                 ).show()
             }
@@ -338,12 +344,14 @@ object Database {
 
             if (task.isSuccessful) {
                 Toast.makeText(
-                    context, "Successully sent a recovery e-mail",
+                    context,
+                    context.resources.getString(R.string.recoveryMailSent),
                     Toast.LENGTH_LONG
                 ).show()
             } else {
                 Toast.makeText(
-                    context, "Failed to send e-mail. Is your e-mail correct?",
+                    context,
+                    context.resources.getString(R.string.recoveryMailFail),
                     Toast.LENGTH_LONG
                 ).show()
             }
@@ -360,12 +368,14 @@ object Database {
         auth.currentUser?.delete()?.addOnCompleteListener { task ->
             if (task.isSuccessful) {
                 Toast.makeText(
-                    context, "Account successfully deleted",
+                    context,
+                    context.resources.getString(R.string.deletedSuccess),
                     Toast.LENGTH_SHORT
                 ).show()
             } else {
                 Toast.makeText(
-                    context, "Something went wrong",
+                    context,
+                    context.resources.getString(R.string.somethingWrong),
                     Toast.LENGTH_SHORT
                 ).show()
             }
