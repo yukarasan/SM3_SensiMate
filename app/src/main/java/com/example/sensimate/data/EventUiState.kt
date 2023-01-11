1371618c5058c138c9193a6318d3a59f08b79eaa
sabirinomar <s211150@dtu.dk>
2023-01-11 17:54:31 +0100

    Small changes

--- app/src/main/java/com/example/sensimate/data/EventUiState.kt
+++ app/src/main/java/com/example/sensimate/data/EventUiState.kt
@@ -4,22 +4,23 @@ import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 
 data class Event(
-    var title: String = "",
-    var description: String = "",
-    var surveyCode: String = "",
-    var eventId: String = "",
-    var location: String = "",
-    var allergens: String = "",
-    var day: String = "",
-    var month: String = "",
-    var year: String = "",
-    var hour: String = "",
-    var minute: String = "",
+    var title : MutableState<String> = mutableStateOf(""),
+    var description: MutableState<String> = mutableStateOf(""),
+    var surveyCode: MutableState<String> = mutableStateOf(""),
+    var eventId: MutableState<String> = mutableStateOf(""),
+    var location: MutableState<String> = mutableStateOf(""),
+    var allergens: MutableState<String> = mutableStateOf(""),
+    var day: MutableState<String> = mutableStateOf(""),
+    var month: MutableState<String> = mutableStateOf(""),
+    var year: MutableState<String> = mutableStateOf(""),
+    var hour: MutableState<String> = mutableStateOf(""),
+    var minute: MutableState<String> = mutableStateOf(""),
     var chosenSurveyCode: MutableState<String> = mutableStateOf("")
 )
 
 data class EventUiState(
     var events: MutableList<Event> = emptyList<Event>().toMutableList(),
     var chosenSurveyId: String = "",
-    var event : Event = Event()
+    var event: Event = Event(),
+
 )
