f5be5304ffd6316350b4d9acba15677a0ea0da47
Yusuf Kara <yusufkarasan@hotmail.com>
2023-01-14 11:15:29 +0100

    Deleted AppViewModel.kt

--- app/src/main/java/com/example/sensimate/MainActivity.kt
+++ app/src/main/java/com/example/sensimate/MainActivity.kt
@@ -1,51 +1,17 @@
 package com.example.sensimate
 
-import android.app.Activity
-import android.content.pm.ActivityInfo
-import android.os.Build
 import android.os.Bundle
-
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.ui.graphics.Color
-import android.view.WindowManager
-import androidx.annotation.RequiresApi
-import androidx.compose.foundation.layout.Column
-import androidx.compose.material.Scaffold
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
-import com.example.sensimate.ui.theme.DarkPurple
-import com.example.sensimate.ui.theme.darkbluegrey
-import android.view.animation.OvershootInterpolator
-import androidx.compose.animation.core.tween
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.size
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.scale
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import androidx.navigation.NavController
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
-import com.example.sensimate.ui.startupscreens.splashscreen.SplashScreen
-import kotlinx.coroutines.delay
 
+/**
+ * MainActivity is the entry point of the app. And it sets the content to be the App composable.
+ */
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            //App(context = LocalContext.current)
             App(context = LocalContext.current)
         }
     }
