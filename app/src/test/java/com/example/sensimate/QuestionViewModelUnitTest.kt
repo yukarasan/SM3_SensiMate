51dee33a94268203eb8b674265d303945568777a
Hussein El-Zein <husseinelzeinprivat@gmail.com>
2023-01-19 16:15:40 +0100

    Now a unit test of QuestionViewModel works

---
+++ app/src/test/java/com/example/sensimate/QuestionViewModelUnitTest.kt
@@ -0,0 +1,31 @@
+package com.example.sensimate
+
+import com.example.sensimate.data.questionandsurvey.MyQuestion
+import com.example.sensimate.data.questionandsurvey.QuestionViewModel
+import junit.framework.TestCase.assertEquals
+import org.junit.Test
+
+/**
+ * This is a unit test that tests the QuestionViewModel() class i created earlier.
+ * Its function that is tested is setCurrentQuestion. If it works, we can set the current question
+ * from surveycreator
+ * @author Hussein El-Zein
+ * */
+class QuestionViewModelUnitTest {
+
+
+    @Test
+    fun questionViewModelUnitTest(){
+
+        val questionvm = QuestionViewModel()
+
+        val question = MyQuestion()
+        question.mainQuestion = "this is a main question"
+
+        questionvm.setCurrentQuestion(question)
+
+
+        assertEquals("this is a main question", questionvm.uiState.value.currentQuestion.mainQuestion)
+    }
+
+}
