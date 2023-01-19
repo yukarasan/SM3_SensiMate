29aaeacbb9e8ad2db12e162cef8b6d13504384bb
Hussein El-Zein <husseinelzeinprivat@gmail.com>
2023-01-19 16:27:45 +0100

    comment on admin test

--- app/src/test/java/com/example/sensimate/AdminUnitTests.kt
+++ app/src/test/java/com/example/sensimate/AdminUnitTests.kt
@@ -1,8 +1,6 @@
 package com.example.sensimate
 
 
-import android.content.Context
-import androidx.test.platform.app.InstrumentationRegistry
 import com.example.sensimate.ui.AdminScreens.AdminViewModel
 import junit.framework.TestCase.assertEquals
 import junit.framework.TestCase.assertNotNull
@@ -12,6 +10,21 @@ import kotlinx.coroutines.test.setMain
 import org.junit.After
 import org.junit.Before
 import org.junit.Test
+
+/**
+
+AdminUnitTests is a class that contains test methods for the AdminViewModel class.
+
+The setUp() method, annotated with @Before, sets the main dispatcher to Unconfined using the ExperimentalCoroutinesApi opt-in.
+
+The CreateEmployee() method, annotated with @Test, creates an instance of the AdminViewModel class and
+calls the createdEmployee method with a parameter of "something@some.some".
+It then asserts that the uiState property is not null and that its mails property contains the expected value.
+The cleanUp() method, annotated with @After and also using the ExperimentalCoroutinesApi opt-in,
+sets the main dispatcher back to Default.
+
+@author Hussein El-Zein
+ */
 
 class AdminUnitTests {
     @OptIn(ExperimentalCoroutinesApi::class)
@@ -29,28 +42,6 @@ class AdminUnitTests {
         assertNotNull(adminViewModel.uiState.value.mails)
         assertEquals("something@some.some", adminViewModel.uiState.value.mails.get(0))
     }
-
-
-/*
-    @Test
-    fun CreateThenDeleteEmployee() {
-        //val context = LocalContext.current
-
-        val adminViewModel = AdminViewModel()
-
-        adminViewModel.createdEmployee("something@some.some")
-
-
-        adminViewModel.clickOnDeleteEmployee("something@some.some", context)
-
-
-        //check that uiState is not null before accessing its properties
-
-
-        assertEquals(0, adminViewModel.uiState.value.mails.size)
-    }
-
- */
 
 
     @OptIn(ExperimentalCoroutinesApi::class)
